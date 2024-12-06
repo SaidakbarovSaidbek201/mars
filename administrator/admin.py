a5989f768416c1cc8b2d261c050e757addeabb2a
SaidakbarovSaidbek <saidguli8880@gmail.com>
2024-12-06 19:24:12 +0500

    yangi ozgarish

--- administrator/admin.py
+++ administrator/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Students, Teachers
+from django.conf import settings
+from django.conf.urls.static import static
 
-# Register your models here.
+admin.site.register(Students)
+admin.site.register(Teachers)
--- administrator/models.py
+++ administrator/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+
+class Students(models.Model):
+    first_name = models.CharField(max_length = 256)
+    last_name = models.CharField(max_length = 256)
+    age = models.IntegerField()
+    group = models.CharField(max_length=255, default='Unknown')
+    coin = models.IntegerField()
+    hobbi = models.CharField(max_length = 256)
+    tolov = models.CharField(max_length = 256)
+    filial = models.CharField(max_length = 256)
+    study_time = models.TimeField()
+    laptop = models.CharField(max_length = 256)
+    
+    class Meta:
+        verbose_name = "O'quvchi"
+        verbose_name_plural = "O'quvchilar"
+
+    def __str__(self):
+        return self.first_name
+
+    
+
+class Teachers(models.Model):
+    first_name = models.CharField(max_length = 256)
+    last_name = models.CharField(max_length = 256)
+    age = models.IntegerField()
+    groups = models.CharField(max_length=255, default='Unknown')
+    salary = models.IntegerField()
+    work = models.CharField(max_length = 256)
+    work_time = models.TimeField()
+    language = models.CharField(max_length = 256)
+    filial = models.CharField(max_length = 256)
+    extra_lesson = models.CharField(max_length = 256)
+
+    
+    class Meta:
+        verbose_name = "O'qitovchi"
+        verbose_name_plural = "O'qitovchilar"
+    def __str__(self):
+        return self.first_name
